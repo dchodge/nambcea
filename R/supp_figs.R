1e70db020a4abf8a9a472cd07d6727d1a2374e15
David Hodgson <davidhodgson@macd0049.lshtm.ac.uk>
2022-10-03 14:02:40 +0100

    CODE UPDATE: 031022

--- R/supp_figs.R
+++ R/supp_figs.R
@@ -1,3 +1,36 @@
+require(dplyr)
+require(tidyr)
+
+require(ggplot2)
+
+labels_month <- c("Jul", "Sep", "Nov", "Jan", "Mar", "May")
+ggplot() +  
+  scale_x_continuous(breaks = seq(1, 12, 2), labels = labels_month, limits = c(0, 12)) + 
+  scale_y_continuous(limits = c(-0.4, 0.7)) +
+  geom_function(fun = "dnorm", args = list(mean = 6, sd = 1), size = 3) + 
+  theme_bw() + theme(axis.text.y = element_blank(), text = element_text(size = 15)) +
+  labs(y = "Transmission Rate")
+
+
+# Load pre-made datasets
+load(file = here::here("data", "inter_model_input", "rsv_data_uk.RData")) # loads ukdata
+load(file = here::here("data", "inter_model_input", "inter_data_uk.RData")) # loads data_inter_uk
+
+age_groups_vec <- c("Time", rep("<6mo", 6), rep("6–11mo", 6), rep("1yr", 1), rep("2-4yrs", 3), rep("5–64yrs", 7), rep("65+yrs", 2))
+df_incidence <- ukdata$observationalData %>% t %>% as.data.frame %>%
+  mutate(details = age_groups_vec) %>% group_by(details) %>% summarise(across(c("V1":"V364"), ~mean(.x))) %>% 
+  pivot_longer("V1":"V364", values_to = "weekly_incidence", names_to = "week") %>%
+  mutate(week = as.numeric(substr(week, 2, 4))) %>% filter(details != "Time") %>% 
+  mutate(details = factor(details, levels = c("<6mo", "6–11mo", "1yr", "2-4yrs", "5–64yrs", "65+yrs")))
+
+
+
+df_incidence %>% 
+  ggplot() + 
+    geom_point(aes(week, weekly_incidence), size = 0.1, shape = 2) + facet_wrap(vars(details)) + 
+    theme_bw() + 
+    labs(x = "Year", y = "Weekly reported samples in RDMS") +
+    scale_x_continuous(breaks = c(seq(1, 400, 52)), labels = 2010:2017) + theme(axis.text.x = element_text(angle = 90))
 
 plot_plane <- function(icer_table_plot, ppd) {
   icer_table_plot <- icer_table_plot_mean
